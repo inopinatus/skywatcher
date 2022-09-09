cb380ad1c5a997e06ba30e8813d3edd4a71041e6
Josh Goodall <inopinatus@hey.com>
2022-09-09 10:04:29 +1000

    merge ShB patch

--- spec/skywatcher_spec.rb
+++ spec/skywatcher_spec.rb
@@ -3,26 +3,26 @@ RSpec.describe Skywatcher do
     expect(Skywatcher::VERSION).not_to be nil
   end
 
-  EorzeaWeather::Data::Locales::MAP.each do |lang, locale|
+  Skywatcher::Data::Locales::MAP.each do |lang, locale|
     weathers = locale.weather.keys
-    EorzeaWeather::Data::Weathers::LIST.each do |weather|
+    Skywatcher::Data::Weathers::LIST.each do |weather|
       it "has defined #{weather} translation in #{lang}" do
         expect(weathers).to be_include(weather)
       end
     end
 
     zones = locale.zone.keys
-    EorzeaWeather::Data::Zones::MAP.each do |zone, _|
+    Skywatcher::Data::Zones::MAP.each do |zone, _|
       it "has defined #{zone} translation in #{lang}" do
         expect(zones).to be_include(zone)
       end
     end
   end
 
-  EorzeaWeather::Data::Zones::MAP.each do |_, zone|
+  Skywatcher::Data::Zones::MAP.each do |_, zone|
     it "has defined weathers at #{zone.id}" do
       zone.rates.each do |chance, weather|
-        expect(EorzeaWeather::Data::Weathers::LIST).to be_include(weather)
+        expect(Skywatcher::Data::Weathers::LIST).to be_include(weather)
       end
     end
   end
